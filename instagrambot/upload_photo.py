7a51b4cf474cc0a2c38a046a53d22076cbc2cefd
Ben Deckey <BDeckey@Bens-MacBook-Pro-3.local>
2017-11-01 17:01:17 -0400

    deleted dirs

--- instagrambot/upload_photo.py
+++
@@ -1,9 +0,0 @@
-from InstagramAPI import InstagramAPI
-user, pwd = 'user', 'pwd'
-
-InstagramAPI = InstagramAPI(user, pwd)
-InstagramAPI.login()  # login
-
-photo_path = 'bob.jpg'
-caption = "Sample photo"
-InstagramAPI.uploadPhoto(photo_path, caption)
