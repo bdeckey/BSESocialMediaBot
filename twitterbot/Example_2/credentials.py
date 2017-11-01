fd308bbc09c99d97cc9f8983f6ce38262c0f9a1d
Ben Deckey <BDeckey@Bens-MacBook-Pro-3.local>
2017-11-01 16:40:10 -0400

    Adding my twitter code.

---
+++ twitterbot/Example_2/credentials.py
@@ -0,0 +1,16 @@
+
+
+# Credentials for your Twitter bot account
+
+# Original script (kept up to date): https://github.com/robincamille/bot-tutorial/blob/master/credentials.py
+
+# 1. Sign into Twitter or create new account
+# 2. Make sure your mobile number is listed at twitter.com/settings/devices
+# 3. Head to apps.twitter.com and select Keys and Access Tokens
+
+CONSUMER_KEY = 'ysKcoO57grOKocyq3eCxz0124'
+CONSUMER_SECRET = 'SBvEJZXY73ChVl4d3lHkdhLuzn6HswTtok05lp6BxYCcyjTXk2'
+
+# Create a new Access Token
+ACCESS_TOKEN = '918558213292871680-yVHzaBz3ofnkXbO3k5xBGkM35UmivKd'
+ACCESS_SECRET = '7Y0M49KdlovG5hWvreLmXmJDybEaVpcZ6sQATX2EimXMo'
